4ac7b1989a0453911824d0024a58308dda235699
Pixel / IKStreamIvo <ivostreaming@gmail.com>
2016-07-17 21:15:52 +0200

    added Unity project

---
+++ Assets/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public string type;
+	public Vector3 playerLocation = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+		playerLocation = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
