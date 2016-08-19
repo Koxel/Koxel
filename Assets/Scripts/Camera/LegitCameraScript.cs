f8ce3fb16880511f8496e344c28652342d8bfee3
NaamloosDT <ryandejonge@outlook.com>
2016-08-19 23:03:32 +0200

    Revert "Moved cam controls to 2nd mouse btn, read desc"
    This reverts commit 3daf368212f0807f6e11d84c9d1b4b46e835ebd2.

--- Assets/Scripts/Camera/LegitCameraScript.cs
+++ Assets/Scripts/Camera/LegitCameraScript.cs
@@ -18,15 +18,18 @@ public class LegitCameraScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0 && i < max)
+        if (Input.GetMouseButton(0))
         {
-            transform.Translate(Vector3.forward * movespeed);
-            i++;
-        }
-        if (Input.GetAxis("Mouse ScrollWheel") < 0 && i > max * -1)
-        {
-            transform.Translate(Vector3.back * movespeed);
-            i--;
+            if (Input.GetAxis("Mouse Y") > 0 && i < max)
+            {
+                transform.Translate(Vector3.forward * movespeed);
+                i++;
+            }
+            if (Input.GetAxis("Mouse Y") < 0 && i > max * -1)
+            {
+                transform.Translate(Vector3.back * movespeed);
+                i--;
+            }
         }
     }
 }
--- Assets/Scripts/TileClickManager.cs
+++ Assets/Scripts/TileClickManager.cs
@@ -7,6 +7,17 @@ public class TileClickManager : MonoBehaviour {
 	public MoveManager manager;
 
 	void OnMouseDown(){
-        manager.moveTo(transform);
+        // Start coroutine waiting for 0.1 sec
+        StartCoroutine(countclick());
+	}
+
+    private IEnumerator countclick()
+    {
+        yield return new WaitForSeconds(0.1f);
+        // If mouse is NOT still down: count as a legit click!
+        if (!Input.GetMouseButton(0))
+        {
+            manager.moveTo(transform);
+        }
     }
 }
