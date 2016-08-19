d95ff95d259e77faf2ab2c65fa5dd2fd0a56056e
NaamloosDT <ryandejonge@outlook.com>
2016-08-19 22:34:05 +0200

    Added camera controls (read desc)
    Hold mouse + forward/back = zoom
    Hold mouse + left/right = rotate
    Every tile click now waits 0.3 seconds to check if mouse is not held
    (believe me, you wont notice :100: )
    
    Also, the reason for the amount of files changed is probably because of
    me using a newer version of Unity..

---
+++ Assets/Scripts/Camera/MainCamObjScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//
+//  This gets loaded onto "maincamobj"
+//
+
+public class MainCamObjScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    public float rotspeed = 5f;
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            if(Input.GetAxis("Mouse X") > 0)
+            {
+                transform.Rotate(Vector3.forward, rotspeed);
+            }
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.Rotate(Vector3.forward, -1 * rotspeed);
+            }
+        }
+	}
+}
--- Assets/Scripts/TileClickManager.cs
+++ Assets/Scripts/TileClickManager.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class TileClickManager : MonoBehaviour {
 
 	public MoveManager manager;
 
 	void OnMouseDown(){
-		manager.moveTo (transform);
+        // Start coroutine waiting for 0.3 sec
+        StartCoroutine(countclick());
 	}
+
+    private IEnumerator countclick()
+    {
+        yield return new WaitForSeconds(0.3f);
+        // If mouse is NOT still down: count as a legit click!
+        if (!Input.GetMouseButton(0))
+        {
+            manager.moveTo(transform);
+        }
+    }
 }
