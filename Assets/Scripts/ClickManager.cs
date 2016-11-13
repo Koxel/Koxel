c1516b00a26b14ae60fa15eb39d92c1b6f924aa3
Pixel / IKStreamIvo <ivostreaming@gmail.com>
2016-11-13 21:13:46 +0100

    I hope this finally fixed life

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickManager : MonoBehaviour {
+
+    public GameObject map;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Debug.Log("hit: " + hitInfo.collider.gameObject.GetComponentInParent<Hex>().x + ", " + hitInfo.collider.GetComponentInParent<Hex>().y);
+
+                map.GetComponent<TileMap>().CalculatePath(hitInfo.collider.gameObject.GetComponentInParent<Hex>().x, hitInfo.collider.GetComponentInParent<Hex>().y);
+            }
+
+
+        }
+
+	}
+}
---
+++ Assets/Scripts/TileType.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class TileType {
+
+    public string name;
+    public GameObject tilePrefab;
+
+    public bool isWalkable;
+    public float movementCost = 1;
+
+}
