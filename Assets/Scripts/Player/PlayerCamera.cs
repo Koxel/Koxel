4792f48f6bc675a809be8ab251bb60d5069d3a56
IKStreamIvo <ivostreaming@gmail.com>
2017-08-21 22:20:43 +0200

    Camera Rotation

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform target;
+    public float followSpeed = 2f;
+    public float rotateSpeed =2f;
+    public KeyCode rotL;
+    public KeyCode rotR;
+
+    private void Start()
+    {
+        if (target == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                target = player.transform;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * followSpeed);
+        }
+
+        float rot = 0f;
+        if (Input.GetKey(rotL))
+        {
+            rot -= 1f;
+        }
+        if (Input.GetKey(rotR))
+        {
+            rot += 1f;
+        }
+
+        transform.Rotate(0, rot*rotateSpeed, 0);
+    }
+}
